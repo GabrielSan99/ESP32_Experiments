ecc45dc3afae681f123f31053fcc8841b9488484
GabrielSan99 <gabriel_j.sanches@hotmail.com>
2022-03-24 14:48:25 -0300

    send_int_value

--- BLE_Array_Transmission/BLE_Receiver_Array/BLE_Receiver_Array.ino
+++ BLE_Array_Transmission/BLE_Receiver_Array/BLE_Receiver_Array.ino
@@ -1,10 +1,3 @@
-/*********
-  Rui Santos
-  Complete instructions at https://RandomNerdTutorials.com/esp32-ble-server-client/
-  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
-  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*********/
-
 #include "BLEDevice.h"
 #include <Wire.h>
 
